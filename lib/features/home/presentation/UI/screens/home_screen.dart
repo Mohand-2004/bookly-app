3a7487d96c8b852f0746a8fa0d8dedf91e9224fb
mohand <mohandelsebaey@gmail.com>
2024-12-20 16:30:24 +0200

    create custom home appbar

--- lib/features/home/presentation/UI/screens/home_screen.dart
+++ lib/features/home/presentation/UI/screens/home_screen.dart
@@ -1,13 +1,22 @@
 import 'package:bookly/core/UI/style/app_colors.dart';
+import 'package:bookly/features/home/presentation/UI/widgets/home_custum_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatelessWidget{
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
       backgroundColor: AppColors.darkPurple,
+      // appBar widget
+      appBar: HomeCustumAppbar(
+        height: 65.h,
+        searchCommand: (text){
+          // TODO: search book  
+        },
+      ),
       body: Center(
         child: Text("Home Screen"),
       ),
